6235e095250cecd4b7ef1e77aedd45036d82cb20
Tim Oliver <20126140@tafe.wa.edu.au>
2025-04-04 11:21:39 +0800

    More tests

--- test_functions.py
+++ test_functions.py
@@ -8,11 +8,13 @@ def test_add():
 
 # uncomment the following test in step 5
 def test_subtract():
-    assert subtract(2, 3) == -1
+   assert subtract(2, 3) == -1
+
+def test_multiply():
+   assert multiply(2, 3) == 6
 
 # uncomment the following test in step 11
 def test_convert_fahrenheit_to_celsius():
    assert f2c(32) == 0
    assert f2c(122) == pytest.approx(50)
-   with pytest.raises(AssertionError):
-       f2c(-600)
+   assert f2c(-600) == pytest.approx(-351.1, abs=0.05)
